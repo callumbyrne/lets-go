8829658097635c53f1dc7d723f723107f08420b0
callumbyrne <callumjbyrne97@gmail.com>
2024-10-11 23:18:23 +1100

    feat: make loggers available application wide and setup db connection

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,22 +1,65 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+// define an application struct to hold the application-wide dependencies for the web app
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+// responsibilities of the main function are limited to:
+// - Parsing the runtime configuration settings for the application
+// - Establishing the dependencies for the handlers
+// - Running the HTTP server
 func main() {
-	mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:web@/snippetbox?parseTime=true", "MySQL data source name")
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	// need to call this before using addr otherwise it will always use the default
+	flag.Parse()
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	// initialize a new instance of our application struct containing the dependencies
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
