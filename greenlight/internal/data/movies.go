da6151583824a33341994a773d1a4192ed3c2a9a
Callum <callumjbyrne97@gmail.com>
2025-02-23 16:15:08 +1100

    feat: movie struct and json formatting

---
+++ greenlight/internal/data/movies.go
@@ -0,0 +1,17 @@
+package data
+
+import "time"
+
+// !All fields in the Movie struct are exported (start with a capital letter)
+// which is necessary for them to be visible to Go's encoding/json package.
+// Any fields that aren't exported won't be included when encoding a struct
+// to JSON
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
