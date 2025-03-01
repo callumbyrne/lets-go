7a30e653fa747853632c337a507a0ffad5471b5b
Callum <callumjbyrne97@gmail.com>
2025-03-01 12:20:07 +1100

    feat: custom json decoding

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -12,10 +12,10 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	// declare anonymous struct to hold expected info
 	// this will be the target decode destination
 	var input struct {
-		Title   string   `json:"title"`
-		Year    int32    `json:"year"`
-		Runtime int32    `json:"runtime"`
-		Genres  []string `json:"genres"`
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
 	}
 
 	err := app.readJSON(w, r, &input)
