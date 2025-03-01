aaddc344107b5cfe29a4073b9ab7453f2982968f
callumbyrne <callumjbyrne97@gmail.com>
2025-03-01 15:21:03 +1100

    feat: field validation

--- greenlight/cmd/api/movies.go
+++ greenlight/cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/callumbyrne/lets-go/greenlight/internal/data"
+	"github.com/callumbyrne/lets-go/greenlight/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,6 +22,20 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.faildValidationResponse(w, r, v.Errors)
 		return
 	}
 
